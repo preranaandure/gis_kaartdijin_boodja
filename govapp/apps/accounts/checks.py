3cfa0be8cf5ddbcc9ae3cc805244b3aad4ace032
shibaken <shibaken@gmail.com>
2025-11-03 15:24:19 +0800

    Add system check to ensure DBCA_Users Geoserver group exists

--- govapp/apps/accounts/checks.py
+++ govapp/apps/accounts/checks.py
@@ -1,8 +1,9 @@
 import sys
 import logging
-from django.core.checks import register, Error, Warning
+from django.core.checks import register, Error
 from django.conf import settings
-# from ledger_api_client.managed_models import SystemGroup
+from govapp.apps.publisher.models.geoserver_roles_groups import GeoServerGroup
+
 
 logger = logging.getLogger(__name__)
 
@@ -12,18 +13,17 @@ def geoserver_group_check(app_configs, **kwargs):
     errors = []
 
     def perform_geoserver_group_check():
-        logger.debug('aho')
-        # for group_name in settings.GROUP_NAME_CHOICES:
-        #     try:
-        #         group, created = SystemGroup.objects.get_or_create(name=group_name)
-        #         if created:
-        #             logger.info(f"SystemGroup: [{group}] has been created.")
-        #         else:
-        #             logger.debug(f"SystemGroup: [{group}] already exists.")
-        #     except Exception as e:
-        #         msg = f"{e}, SystemGroup: [{group_name}]"
-        #         errors.append(Error(msg))
-        #         logger.error(msg)
+        for group_name in settings.CUSTOM_GEOSERVER_GROUPS:
+            try:
+                group, created = GeoServerGroup.objects.get_or_create(name=group_name)
+                if created:
+                    logger.info(f"GeoServerGroup: [{group}] has been created.")
+                else:
+                    logger.debug(f"GeoServerGroup: [{group}] already exists.")
+            except Exception as e:
+                msg = f"{e}, GeoServerGroup: [{group_name}]"
+                errors.append(Error(msg))
+                logger.error(msg)
 
     if sys.argv and ('migrate' in sys.argv or 'makemigrations' in sys.argv or 'showmigrations' in sys.argv or 'sqlmigrate' in sys.argv):
         pass
